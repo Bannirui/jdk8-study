5fdeb44866fa7d47d66aea094bf0888906410730
Bannirui <1652959004@qq.com>
2021-01-06 22:51:35 +0800

    更新jdk8源码包 线程池

--- src/test/ThreadPoolTest.java
+++ src/test/ThreadPoolTest.java
@@ -15,7 +15,7 @@ public class ThreadPoolTest {
      * @author dingrui
      * @date 2021/1/5
      * @return
-     * @description
+     * @description 手动创建线程池 将任务执行分配给核心线程执行
      */
     @Test
     public void test1() {
@@ -33,8 +33,45 @@ public class ThreadPoolTest {
                 },
                 new ThreadPoolExecutor.AbortPolicy()
         );
-        //提交一个任务
+        // 提交一个任务
         executor.execute(() -> System.out.println("ok"));
+    }
+
+    /**
+     * @author dingrui
+     * @date 2021/1/5
+     * @return
+     * @description 手动创建线程池 将任务执行分配给非核心线程执行
+     */
+    @Test
+    public void test5() {
+        // 手动创建线程池
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                0,
+                4,
+                3L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(2),
+                r -> {
+                    System.out.println("创建线程：" + r.hashCode());
+                    //线程命名
+                    return new Thread(r, "threadPool" + r.hashCode());
+                },
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+        // 设置coreThreadTimeOut这个属性可以让核心线程在空闲的时候也死亡 也就是说没有任务空闲的时候线程池不维护任何一个线程
+        executor.allowCoreThreadTimeOut(false);
+        // 提交一个任务
+        for (int i = 0; i < 6; i++) {
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(1000 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("ok");
+            });
+        }
     }
 
     /**
@@ -68,6 +105,32 @@ public class ThreadPoolTest {
     @Test
     public void test3() {
         SecurityManager securityManager = System.getSecurityManager();
+        System.out.println();
+    }
+
+    /**
+     * @author dingrui
+     * @date 2021/1/5
+     * @return
+     * @description ctl 高3位存储线程池工作状态 低29位存储工作线程数量
+     */
+    @Test
+    public void test4() {
+        // 移位操作
+        int COUNT_BITS = 29;
+        // -1这个32位整型的高3位表示running状态
+        int running = -1 << COUNT_BITS;
+        // 工作线程数量0个
+        int workerCount = 0;
+        // 工作线程数量上限
+        int CAPACITY = (1 << COUNT_BITS) - 1;
+        // 计算ctl
+        int ctl = running | workerCount;
+        // 从ctl中计算出线程池状态
+        int runStateOf = ctl & ~CAPACITY;
+        // 从ctl中计算出工作线程数量
+        int workerCountOf = ctl & CAPACITY;
+
         System.out.println();
     }
 }
