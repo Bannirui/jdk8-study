6be38c75cc52279596e6f43ce3f226b3647b1cf6
Bannirui <1652959004@qq.com>
2021-03-15 15:30:58 +0800

    jdk8源码学习 Throwable

---
+++ src/test/basicwrapper/ThrowTest.java
@@ -0,0 +1,17 @@
+package test.basicwrapper;
+
+import org.junit.Test;
+
+/**
+ *@author dingrui
+ *@date 2021-03-15
+ *@description
+ */
+public class ThrowTest {
+
+    @Test
+    public void test1() {
+        Throwable t = new Throwable();
+        System.out.println();
+    }
+}
